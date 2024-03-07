e9a2fd07bb707bafd2fe8788424aef9d670df607
septianhari <aribarno86@gmail.com>
2024-03-07 20:58:24 +0700

    add file if.go

---
+++ if.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Contoh 1
+	angka := 10
+
+	if angka > 5 {
+		fmt.Println("Angka lebih besar dari 5")
+	} else {
+		fmt.Println("Angka kurang dari atau sama dengan 5")
+	}
+
+}
